d219f25c3f2d0c289258cd5cbc9f02a0dc4a7532
root <kevintroko@gmail.com>
2020-11-18 03:40:57 +0000

    8. Obtener tercer parametro de Estado e imprimir

--- kbot.rb
+++ kbot.rb
@@ -1,10 +1,11 @@
 class KleerBot
-    def calcular(cantidad, precio)
+    def calcular(cantidad, precio, estado)
         total = cantidad.to_i * precio.to_i
         puts "# #{cantidad} * $#{precio} = $#{total}"
+        puts estado
     end
 end
 
 kleer_bot = KleerBot.new()
 
-puts kleer_bot.calcular(ARGV[0], ARGV[1])
+puts kleer_bot.calcular(ARGV[0], ARGV[1], ARGV[2])
